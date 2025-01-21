9e6caf267cfbc480fd94603c43eb6595342195c2
Majawat <12058855+Majawat@users.noreply.github.com>
2025-01-21 07:09:30 -0600

    fix dropdown click hiding navbar

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   var navbar = document.getElementById("navbarSupportedContent");
   var links = navbar.querySelectorAll(".nav-link, .dropdown-item");
+  var dropdownToggles = navbar.querySelectorAll(".dropdown-toggle");
 
   links.forEach(function (link) {
-    link.addEventListener("click", function () {
-      if (navbar.classList.contains("show")) {
+    link.addEventListener("click", function (event) {
+      // Check if the clicked link is a dropdown toggle
+      var isDropdownToggle = Array.from(dropdownToggles).includes(event.target);
+
+      if (navbar.classList.contains("show") && !isDropdownToggle) {
         var bsCollapse = new bootstrap.Collapse(navbar, {
           toggle: false,
         });
