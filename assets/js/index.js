74e87ef8f30e2c235006073500dee454bbf4d5c8
Majawat <12058855+Majawat@users.noreply.github.com>
2025-01-28 18:02:30 -0600

    remove unneeded stuff

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,24 +4,3 @@ document.addEventListener("DOMContentLoaded", function () {
     link.setAttribute("target", "_blank");
   });
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  const links = document.querySelectorAll('a[href^="http"]');
-  links.forEach((link) => {
-    link.setAttribute("target", "_blank");
-  });
-
-  const offcanvasElement = document.getElementById("bdNavbar");
-  if (offcanvasElement) {
-    const offcanvas = new bootstrap.Offcanvas(offcanvasElement);
-    offcanvasElement.addEventListener("click", function (event) {
-      const target = event.target;
-      if (
-        target.tagName === "A" &&
-        target.getAttribute("href").startsWith("#")
-      ) {
-        offcanvas.hide();
-      }
-    });
-  }
-});
