e2a61050879e25acab015dd1d856d40dc764c8d8
Majawat <12058855+Majawat@users.noreply.github.com>
2025-02-10 13:29:49 -0600

    fix up armies page a bit, make cards the same size

--- assets/js/load-army-bio.js
+++ assets/js/load-army-bio.js
@@ -25,13 +25,12 @@ document.addEventListener("DOMContentLoaded", () => {
         let armyContent = `
         <section
           id="${army.armyURL}"
-          class="mb-5 col-12 col-md-6 col-lg-5 nav-scroll-top mx-auto"
+          class="col-12 col-md-6 col-lg-5 nav-scroll-top"
         >`;
-        armyContent += `<div class="card border-secondary">`;
+        armyContent += `<div class="card border-secondary h-100">`;
         armyContent += `            <img
               src="${army.image}"
               class="card-img-top img-fluid"
-              loading="eager"
               style="
                 width: 100%;
                 height: 250px;
@@ -41,35 +40,35 @@ document.addEventListener("DOMContentLoaded", () => {
               alt="The ${army.armyName} Army"
             />`;
         armyContent += `<div class="card-header bg-gradient bg-dark text-light">
-              <h2 class="mb-0">${army.armyName}</h2>`;
+              <h2 class="h4 mb-20">${army.armyName}</h2>`;
 
         if (army.faction[0].alias) {
           for (faction of army.faction) {
             console.log(faction.alias, faction.name);
-            armyContent += `<span class="badge me-1 text-bg-info" data-bs-toggle="tooltip" data-bs-title="${faction.name}">${faction.alias}</span>`;
+            armyContent += `<span class="badge me-1 text-bg-secondary" data-bs-toggle="tooltip" data-bs-title="${faction.name}">${faction.alias}</span>`;
           }
         } else {
           for (faction of army.faction) {
-            armyContent += `<span class="badge me-1 text-bg-info">${faction.name}</span>`;
+            armyContent += `<span class="badge me-1 text-bg-secondary">${faction.name}</span>`;
           }
         }
 
         armyContent += `</div>`;
 
-        armyContent += `<div class="card-body">
-              <h3 class="card-title text-secondary">
+        armyContent += `<div class="card-body d-flex flex-column">
+              <h3 class="card-title h5 text-secondary">
                 ${army.playerTitle}
                 <span class="text-body fw-bold">${army.player}</span>
               </h3>
-              <p class="lead mb-3 text-muted">
+              <p class="lead mb-3 text-muted fs-6">
                 ${army.tagline}
               </p>`;
-        armyContent += `<p>${army.summary}</p>`;
+        armyContent += `<p class="flex-grow-1">${army.summary}</p>`;
 
         if (army.armyURL == null) {
-          armyContent += `<div class="text-end mt-3"> <a href="" class="btn btn-outline-primary disabled">View Full Army Details</a> </div>`;
+          armyContent += `<div class="text-end mt-auto"> <a href="" class="btn btn-outline-primary disabled">View Full Army Details</a> </div>`;
         } else {
-          armyContent += `<div class="text-end mt-3"> <a href="${army.armyURL}.html" class="btn btn-primary">View Full Army Details</a> </div>`;
+          armyContent += `<div class="text-end mt-auto"> <a href="${army.armyURL}.html" class="btn btn-primary">View Full Army Details</a> </div>`;
         }
 
         armiesContainer.innerHTML += armyContent;
